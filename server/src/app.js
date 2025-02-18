b278af0182a32289f007b373e077855a20965ab1
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-02-18 11:09:40 +0530

    basci backend setup

---
+++ server/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+const app=express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+//routes import
+import userRoute from "./routes/user.route.js";
+import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js";
+import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
+
+//router declaration
+app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
+
+export {app}
